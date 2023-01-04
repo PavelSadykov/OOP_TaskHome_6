56e0d837fae684bbb2d28439cf539e210a5cb4f6
Pavel Sadykov <2439747@gmail.com>
2023-01-04 22:10:43 +0500

    Create class WClass.cpp

---
+++ class WClass.cpp
@@ -0,0 +1,14 @@
+//
+//  class WClass.cpp
+//  ДЗ 6 ООП
+//
+//  Created by Павел on 04.01.2023.
+//
+
+#include "class WClass.hpp"
+
+WClass::WClass(int _x, int _y) : x (_x), y (_y){};
+
+    
+   
+  
